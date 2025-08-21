89a382d97e75bf32859b5ef904af67a4cc90b94f
Leo Donato <kqjut3@gmail.com>
2025-08-21 11:20:13 -0300

    feat: ajustando aplicação para ambiente de host

--- SnakeUtilities/Program.cs
+++ SnakeUtilities/Program.cs
@@ -1,3 +1,4 @@
+using QuestPDF;
 using QuestPDF.Infrastructure;
 using SnakeUtilities.Modelos.EtiquetaCorreios;
 using SnakeUtilities.Servicos;
@@ -13,20 +14,20 @@ builder.Services.AddScoped<IServicoCorreios, ServicoCorreios>();
 
 builder.Services.AddHttpClient();
 
-QuestPDF.Settings.License = LicenseType.Community;
+Settings.License = LicenseType.Community;
 
 var corsPadrao = "CorsPadrao";
+var origensPermitidas = builder.Configuration.GetSection("OriginsPermitidas").Get<string[]>();
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: corsPadrao,
+    options.AddPolicy(corsPadrao,
         policy =>
         {
-            // Para desenvolvimento, você pode ser mais permissivo
-            // Lembre-se de ser mais restritivo em produção!
-            policy.WithOrigins("http://localhost:3000") // A URL do seu frontend Vue
-                .AllowAnyHeader()
-                .AllowAnyMethod();
+            if (origensPermitidas?.Length > 0)
+                policy.WithOrigins(origensPermitidas)
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
         });
 });
 
@@ -35,22 +36,38 @@ var app = builder.Build();
 app.UseCors(corsPadrao);
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+if (app.Environment.IsDevelopment()) app.MapOpenApi();
 
-app.UseHttpsRedirection();
+app.MapGet("/correios/buscar-cep/{cep}",
+    async (string cep, IServicoCorreios servico) =>
+    {
+        try
+        {
+            return Results.Ok(await servico.ObterEndereco(cep));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+            return Results.Problem("Ocorreu um erro ao buscar o CEP.", statusCode: 500);
+        }
+    });
 
-app.MapGet("/correios/buscar-cep/{cep}", async (string cep, IServicoCorreios servico) => await servico.ObterEndereco(cep));
+app.MapPost("/correios/etiqueta-pdf",
+    (Etiqueta etiqueta, IServicoCorreios servico) =>
+    {
+        try
+        {
+            var bytes = servico.GerarPdf(etiqueta);
 
-app.MapPost("/correios/etiqueta-pdf", (Etiqueta etiqueta, IServicoCorreios servico) =>
-{
-    var bytes = servico.GerarPdf(etiqueta);
-    
-    var nomeArquivo = $"etiqueta_{DateTime.UtcNow:yyyyMMdd_HHmmss}.pdf";
-    
-    return Results.File(bytes, "application/pdf", nomeArquivo);
-});
+            var nomeArquivo = $"etiqueta_{DateTime.UtcNow:yyyyMMdd_HHmmss}.pdf";
+
+            return Results.File(bytes, "application/pdf", nomeArquivo);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+            return Results.Problem("Ocorreu um erro ao gerar a etiqueta.", statusCode: 500);
+        }
+    });
 
 app.Run();
